b64576a0077712b8412ad8265b7ad21d6b510592
aryan68125 <aryan68125@gmail.com>
2021-09-06 08:04:29 +0530

    login page added

---
+++ To_do_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+    path('', views.home, name='home'),
+]
--- To_do_app/views.py
+++ To_do_app/views.py
@@ -1,3 +1,27 @@
+from django.urls import path
 from django.shortcuts import render
 
-# Create your views here.
+'''
+add the basic functionality of the database to this view.py module
+'''
+from . import models
+
+#from To_do_app.models import 
+
+'''
+import timezone from django web framework
+'''
+from django.utils import timezone
+
+'''
+pip3 install requests before importing requests
+'''
+import requests
+
+'''
+this import helps us redirect to a new page with different url from the current page
+'''
+from django.shortcuts import redirect
+
+def home(request):
+    return render(request, 'main/index.html')
